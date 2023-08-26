8dbd53037f5eea64ebb59558e132f9781a409dc3
Fausto Zaruma <fazatojuni@gmail.com>
2023-08-25 22:44:49 -0400

    Style: Adding styles and animation for navbar elements

--- client/src/Navigation.tsx
+++ client/src/Navigation.tsx
@@ -1,29 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "./styles/Navigation.css";
 
-function Navigation(){
-    return (
-      <div>
-        <header>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="/">FlavourOfHealth</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/recipes">Recipes</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/nutrition">Nutrition</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/services">Services</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/book-online">Book Online</Link></li>
-              </ul>
-            </div>
-          </nav>
-        </header>
-      </div>
-    );
-  }
+function Navigation() {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light ps-5">
+          <Link className="navbar-brand" to="/">
+            FlavourOfHealth
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-center"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav topBottomBordersOut">
+              <li className="nav-item">
+                <Link className="nav-link px-3" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link px-3" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link px-3" to="/recipes">
+                  Recipes
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link px-3" to="/nutrition">
+                  Nutrition
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link px-3" to="/services">
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link px-3" to="/book-online">
+                  Book Online
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+}
 
 export default Navigation;
--- client/src/Recipe.tsx
+++ client/src/Recipe.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import Navigation from './Navigation';
 function Recipe() {
   return (
