66c7cccdd31a83bc7463f6977b105d05f1f954b9
Fausto Zaruma <fazatojuni@gmail.com>
2023-09-17 12:31:47 -0400

    T2

--- server/src/index.ts
+++ server/src/index.ts
@@ -51,7 +51,7 @@ app.use(express.static(path.join(__dirname, '..', 'client')));
 
 //Catch all
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+    res.sendFile(path.join(__dirname, '..','..', 'client', 'index.html'));
   });
 
 //admin Login
