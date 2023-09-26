cdd18d42a67752b9c9844c8e8f4de4e04ed5a377
Fausto Zaruma <fazatojuni@gmail.com>
2023-09-26 18:19:55 -0400

    Mocked design for recipe.

--- client/src/components/Recipe.tsx
+++ client/src/components/Recipe.tsx
@@ -2,18 +2,37 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { Card, Container } from "react-bootstrap";
+import "../styles/Recipe.css";
 
 interface RecipeType {
   _id: string;
   name: string;
-  image: string;
-  description: string;
+  images: [];
+  descriptions: [];
   tag: string;
 }
 
 function Recipe() {
   const [recipe, setRecipe] = useState<RecipeType | null>(null);
   const { id } = useParams<{ id: string }>();
+  // TODO: delete mockResponse after updating DB models to support arrays
+
+  const images = [
+    "https://assets.elgourmet.com/wp-content/uploads/2023/03/s-ndw_AfsS859PRoHcJpKwd6r4Me0FUBDtCg-1024x683.png.webp",
+    "https://assets.elgourmet.com/wp-content/uploads/2023/03/s-ndw_AfsS859PRoHcJpKwd6r4Me0FUBDtCg-1024x683.png.webp",
+    "https://assets.elgourmet.com/wp-content/uploads/2023/03/s-ndw_AfsS859PRoHcJpKwd6r4Me0FUBDtCg-1024x683.png.webp",
+  ];
+  const paragraphs = [
+    " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget purus dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam fermentum lectus eu mauris auctor interdum ac eu nunc. Curabitur dignissim ante ligula, quis laoreet felis lobortis id. Sed scelerisque, metus sit amet elementum sollicitudin, enim purus molestie nisl, a interdum neque lectus sit amet sem. Maecenas est arcu, malesuada vel sapien ut, interdum mattis ante. Etiam posuere eros eget lorem pellentesque blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas aliquam enim eu nisi rhoncus semper. Aliquam augue dui, posuere maximus elit et, viverra sodales neque. ",
+    " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget purus dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam fermentum lectus eu mauris auctor interdum ac eu nunc. Curabitur dignissim ante ligula, quis laoreet felis lobortis id. Sed scelerisque, metus sit amet elementum sollicitudin, enim purus molestie nisl, a interdum neque lectus sit amet sem. Maecenas est arcu, malesuada vel sapien ut, interdum mattis ante. Etiam posuere eros eget lorem pellentesque blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas aliquam enim eu nisi rhoncus semper. Aliquam augue dui, posuere maximus elit et, viverra sodales neque. ",
+    " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget purus dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam fermentum lectus eu mauris auctor interdum ac eu nunc. Curabitur dignissim ante ligula, quis laoreet felis lobortis id. Sed scelerisque, metus sit amet elementum sollicitudin, enim purus molestie nisl, a interdum neque lectus sit amet sem. Maecenas est arcu, malesuada vel sapien ut, interdum mattis ante. Etiam posuere eros eget lorem pellentesque blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas aliquam enim eu nisi rhoncus semper. Aliquam augue dui, posuere maximus elit et, viverra sodales neque. ",
+  ];
+
+  const mockResponse = {
+    images,
+    paragraphs,
+    title: "MockTitle",
+  };
 
   useEffect(() => {
     axios
@@ -27,25 +46,20 @@ function Recipe() {
   }, [id]);
 
   return (
-    <Container style={{ marginTop: "20px" }}>
-      {recipe ? (
-        <>
-          <h1 style={{ marginBottom: "20px", textAlign: "center" }}>{recipe.name}</h1>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={recipe.image}
-            />
-            <Card.Body style={{ textAlign: "left" }}>
-              <Card.Text dangerouslySetInnerHTML={{ __html: recipe.description.replace(/\n/g, '<br />') }} />
-            </Card.Body>
-            <Card.Footer>
-              <small>{recipe.tag}</small>
-            </Card.Footer>
-          </Card>
-        </>
-      ) : (
-        <p>Loading...</p>
+    <Container className="recipe-container" style={{ marginTop: "20px" }}>
+      <h2>{mockResponse.title}</h2>
+      {mockResponse.images.map((img, index) =>
+        (index + 1) % 2 == 0 ? (
+          <div className="pair" key={index}>
+            <img src={img} alt={`Image ${index + 1}`} />
+            <p>{mockResponse.paragraphs[index]}</p>
+          </div>
+        ) : (
+          <div className="pair" key={index}>
+            <p>{mockResponse.paragraphs[index]}</p>
+            <img src={img} alt={`Image ${index + 1}`} />
+          </div>
+        )
       )}
     </Container>
   );
